452a38141a55b3025e44ac2e05c8310c72b9b28b
Trevor Nederlof <trevor.nederlof@posit.co>
2024-01-05 15:19:49 +0000

    Add plumber-loan-model example

---
+++ R/plumber-loan-model/README.md
@@ -0,0 +1,55 @@
+# Loan Risk Prediction Model using a Plumber API
+
+A loan risk prediction model created using [tidymodels](https://www.tidymodels.org/)/[xgboost](https://xgboost.readthedocs.io/en/stable/R-package/xgboostPresentation.html) surfaced as an API using [plumber](https://www.rplumber.io/).
+
+![screenshot of Plumber API docs](screenshot.png)
+
+## Usage
+
+Setup the `renv` environment:
+
+```r
+renv::activate()
+renv::restore()
+```
+
+To run the API locally:
+
+Open the `api/plumber.R` file in the IDE and click the "Run API" button.
+
+## Deployment
+
+### Push Button
+
+Open `api/plumber.R` and use the blue publish icon in the upper right corner of the IDE code pane.
+
+### rsconnect package
+
+You can also deploy using the rsconnect package:
+
+```
+rsconnect::deployAPI(
+  api = "api",
+  appFiles = c("plumber.R"),
+  appTitle = "Loan Risk Model Plumber"
+)
+```
+
+### Git-backed
+
+Update the code, and then run:
+
+```r
+rsconnect::writeManifest("api")
+```
+
+Commit the new `manifest.json` file to the git repo along with the code.
+
+## Training
+
+This example has a pre-created `model` directory. If you wish to re-run the training then run the `train.R` script.
+
+## Resources
+
+[Posit Connect User Guide: Plumber](https://docs.posit.co/connect/user/plumber/)
+[Tidymodels Getting Started](https://www.tidymodels.org/start/)
---
+++ R/plumber-loan-model/api/plumber.R
@@ -0,0 +1,62 @@
+# plumber.R
+library(tidyverse)
+library(tidymodels)
+library(finetune)
+library(xgboost)
+library(arrow)
+library(plumber)
+library(bundle)
+
+#* @apiTitle Loan Risk Classification API
+#* @apiDescription Classifies a loan as A, B, C, or D depending on the risk
+#*     profile of the applicant. A is the least risky and D is the most risky.
+
+#* Predict a loan status
+#* @param age:int Age of loan holder
+#* @param district_id:int District ID of loan holder
+#* @param amount:numeric Amount of loan in USD
+#* @param duration:int Duration of loan in months
+#* @param fico:int FICO score of loan applicant
+#* @post /predict
+function(age=47L, district_id=63L, amount=5000, duration=12L, fico=750){
+  
+  mod_bundle <- readRDS("../model/xgb_final_fit.rds")
+  xgb_final_fit <- unbundle(mod_bundle)
+
+  predict_df <- tibble(
+    age = as.integer(age),
+    district_id = as.integer(district_id),
+    amount = as.numeric(amount),
+    duration = as.integer(duration),
+    fico = as.integer(fico)
+  )
+ 
+  risk_category <- predict(xgb_final_fit, predict_df) |>
+    pull(.pred_class) |>
+    as.character()
+}
+
+#* List District Information
+#* @param city:string City name
+#* @param state_abbrev:string State abbreviation
+#* @param region:string Region name
+#* @param division:string Division name
+#* @get /districts
+function(city="", state_abbrev="", region="", division="") {
+  district_df <- read_parquet("../data/district.parquet") |>
+    mutate(district_id = as.integer(district_id))
+
+  if (city != "") {
+    district_df <- filter(district_df, city == !!city)
+  }
+  if (state_abbrev != "") {
+    district_df <- filter(district_df, state_abbrev == !!state_abbrev)
+  }
+  if (region != "") {
+    district_df <- filter(district_df, region == !!region)
+  }
+  if (division != "") {
+    district_df <- filter(district_df, division == !!division)
+  }
+  return(district_df)
+}
